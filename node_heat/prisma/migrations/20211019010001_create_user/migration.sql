b1985c6235d91a29636e50741e980801206cd912
Leandro Guaraldi <leandro_319@yahoo.com.br>
2021-10-18 23:51:34 -0300

    Aula 01-Node

---
+++ node_heat/prisma/migrations/20211019010001_create_user/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "github_id" INTEGER NOT NULL,
+    "avartar_url" TEXT NOT NULL,
+    "login" TEXT NOT NULL
+);
